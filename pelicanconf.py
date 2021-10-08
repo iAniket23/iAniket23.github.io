2af422702d4124fa5f9fc6863faa8716e2d58e35
Aniket Mishra <ianiket23@gmail.com>
2021-10-08 11:43:18 -0600

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,15 +18,15 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# # Blogroll
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+          ('LinkedIn', 'www.linkedin.com/in/ianiket23'),)
 
 DEFAULT_PAGINATION = 10
 
