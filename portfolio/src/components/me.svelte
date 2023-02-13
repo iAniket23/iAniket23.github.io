eb4f9f61dd9f5a9211b39daf1816a096c28c0a05
Aniket Mishra <mishra2@ualberta.ca>
2023-02-12 23:40:44 -0700

    gif ready

---
+++ portfolio/src/components/me.svelte
@@ -0,0 +1,17 @@
+<div class="img"></div>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    margin-bottom: 0%;
+    }
+div.img {
+    background-image: url('img/port.gif');
+    background-position: bottom;
+    background-repeat: no-repeat;
+    background-size: contain;
+    width: 100vw;
+    height: 50vh;
+    }
+
+</style>
