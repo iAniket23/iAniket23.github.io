8647544ea1d00737c49f4f314931fb464958d42d
Aniket Mishra <mishra2@ualberta.ca>
2023-02-23 14:52:44 -0700

    Update gh-pages.js

--- portfolio/gh-pages.js
+++ portfolio/gh-pages.js
@@ -4,7 +4,7 @@ publish(
   'build', // path to public directory
   {
     branch: 'gh-pages',
-    repo: 'https://github.com/goldentoaste/goldentoaste.github.io.git', // Update to point to your repository
+    repo: 'https://github.com/iAniket23/iAniket23.github.io.git', // Update to point to your repository
     user: {
       name: 'iAniket23', // update to use your name
       email: 'ianiket23@gmail.com' // Update to use your email
