a77343e0b9bbc154acedf84f609cde2cee88e8a2
Aniket Mishra <mishra2@ualberta.ca>
2023-02-23 09:30:10 -0700

    deployment

---
+++ portfolio/gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from 'gh-pages';
+
+publish(
+ 'build', // path to public directory
+ {
+  branch: 'gh-pages',
+  repo: 'https://github.com/iAniket23/iAniket23.github.io.git', // Update to point to your repository
+  user: {
+   name: 'iAniket23', // update to use your name
+   email: 'ianiket23@gmail.com' // Update to use your email
+  },
+  dotfiles: true
+  },
+  () => {
+   console.log('Deploy Complete!');
+  }
+);
