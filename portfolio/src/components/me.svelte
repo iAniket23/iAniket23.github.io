3eff660f6a4e01910678eabca7a15a9f6db9e3c7
Aniket Mishra <mishra2@ualberta.ca>
2023-02-23 17:34:03 -0700

    done

--- portfolio/src/components/me.svelte
+++ portfolio/src/components/me.svelte
@@ -5,7 +5,7 @@
     margin: 0 auto;
     }
 div.img {
-    background-image: url('img/bgport.gif');
+    background-image: url('../components/bgport.gif');
     background-position: center;
     background-repeat: no-repeat;
     background-size: contain;
