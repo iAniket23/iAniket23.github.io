a7c5bca9f013c0e7154d7a43bdf83f4e01453be1
iAniket23 <ianiket23@gmail.com>
2025-04-22 17:27:33 -0600

    Updates

--- app/immutable/chunks/2-43b9979d.js
+++
@@ -1,4 +0,0 @@
-import { default as default2 } from "../components/pages/_page.svelte-ce80147f.js";
-export {
-  default2 as component
-};
---
+++ app/immutable/chunks/2-71c80c1d.js
@@ -0,0 +1,4 @@
+import { default as default2 } from "../components/pages/_page.svelte-9f345281.js";
+export {
+  default2 as component
+};
